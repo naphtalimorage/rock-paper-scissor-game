b5f8349f979112d7a120b03890bcd2d9ce1a583f
Naphtali Morage <naphtalimorage56@example.com>
2024-10-14 21:09:04 +0300

    added some css styling and styled some user interface

--- src/App.js
+++ src/App.js
@@ -1,69 +1,79 @@
 import "./App.css";
-import React, { useEffect } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 
-const App = ()  => {
-  const [playerChoice, setPlayerChoice] = React.useState("");
-  const[computerChoice, setComputerChoice] = React.useState("");
-  const [playerScore, setPlayerScore] = React.useState(0);
-  const [computerScore, setComputerScore] = React.useState(0);
+const App = () => {
+  const [playerChoice, setPlayerChoice] = useState("");
+  const [computerChoice, setComputerChoice] = useState("");
+  const [playerScore, setPlayerScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+  const[message, setMessage] = useState("");
 
   const getRandomChoice = () => {
-    const choices = ["ROCK", "PAPER", "SCISSORS"];
-    return choices[Math.floor(Math.random() * choices.length)];
-  }
+    const choices = ["Rock", "Paper", "Scissors"];
+    const randomChoices = [Math.floor(Math.random() * choices.length)];
+    const computerChoices = choices[randomChoices];
+    setComputerChoice(computerChoices);
+  };
 
-  setComputerChoice(getRandomChoice);
-    const determineWinner = () => {
-      if (playerChoice === "" || computerChoice === "") {
-        return;
-      }  else if (playerChoice === computerChoice) {
-        return "It's a tie!";
-      } else if ((playerChoice === "ROCK" && computerChoice === "SCISSORS") ||
-          (playerChoice === "PAPER" && computerChoice === "ROCK") ||
-          (playerChoice === "SCISSORS" && computerChoice === "PAPER")) {
-            setPlayerScore(playerScore + 1);
-  
-        return "You win!";
-      } else {
-        setComputerScore(computerScore + 1);
-        return "Computer wins!";
-      }
+  const determineWinner = () => {
+     if (playerChoice === computerChoice) {
+      setMessage("It's a tie!");
+      setComputerScore(computerScore + 1);
+      setPlayerChoice(playerScore + 1);
+    } else if (
+      (playerChoice === "Rock" && computerChoice === "Scissors") &&
+      (playerChoice === "Paper" && computerChoice === "Rock") &&
+      (playerChoice === "Scissors" && computerChoice === "Paper")
+    ) {
+      setPlayerScore(playerScore + 1);
+      setMessage("You win!");
+    } else {
+      setComputerScore(computerScore + 1);
+      setMessage("Computer wins!");
     }
-    
+  };
 
-    useEffect(() => {
-      if(playerChoice) {
-          const computerNewChoice = getRandomChoice();
-          setComputerChoice(computerNewChoice);
-          determineWinner();
-      }
-    }, [playerChoice]);
+  useEffect(() => {
+    if (playerChoice) {
+      determineWinner();
+      getRandomChoice();
+    }
+  }, [playerChoice]);
 
-  
- 
-    return(
-        <div>
-            <h1>Welcome to Rock, Paper and Scissor Game</h1>
-              <div>
-                <button onClick={() => setPlayerChoice("ROCK")}>
-                    Rock
-                </button>
-                <button onClick={() => setPlayerChoice("PAPER")}>
-                    Paper
-                </button>
-                <button onClick={() => setPlayerChoice("SCISSORS")}>
-                    Scissors
-                </button>
-            </div>
-            <div>
-                <p>Your Choice: {playerChoice}</p>
-                <p>Computer Choice: {computerChoice}</p>
-                <p>Player Score: {playerScore}</p>
-                <p>Computer Score: {computerScore}</p>
-            </div> 
+  return (
+    <div className="container">
+      <div className="main">
+        <h1>Welcome to Rock, Paper and Scissor Game</h1>
+        <div className="button-container">
+          <button name="rock" onClick={() => setPlayerChoice("Rock")}>
+            Rock
+          </button>
+          <button
+            name="paper"
+            value={playerChoice}
+            onClick={() => setPlayerChoice("Paper")}
+          >
+            Paper
+          </button>
+          <button
+            name="scissors"
+            value={playerChoice}
+            onClick={() => setPlayerChoice("Scissors")}
+          >
+            Scissors
+          </button>
         </div>
-    )
-}
-
+        <span className="message">{<p>{message}</p>}</span>
+        <div className="result-container">
+          <p>Your Choice:<span className="values1">{playerChoice}</span> </p>
+          <p>Computer Choice: <span className="values2">{computerChoice}</span> </p>
+          <p>Player Score: <span className="values3">{playerScore}</span> </p>
+          <p>Computer Score: <span className="values4">{computerScore}</span></p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
